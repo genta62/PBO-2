a4bc709c5b9bc40bcd1371a9ee493f2e98be90e7
Genta Kurniawan <muhammad_genta_29rpl@student.smktelkom-mlg.sch.id>
2021-03-19 19:15:14 +0700

    first commit

---
+++ encapsiswa/TestSiswa.java
@@ -0,0 +1,19 @@
+package encapsiswa;
+
+/**
+ *
+ * @author genta
+ */
+public class TestSiswa {
+    public static void main(String[] args){
+    EncapSiswa siswa = new EncapSiswa();
+    siswa.setName("agus");
+    siswa.setAge(20);
+    siswa.setAddress("Malang");
+        System.out.println("nama: "+siswa.getName()
+        + " alamat "+siswa.getAddress()+ " berumur "+siswa.getAge()
+        + " tahun");
+
+    }
+    
+}
