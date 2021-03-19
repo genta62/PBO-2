c45e9fc96e487dbf073a0fe055030488bcedb1eb
Genta Kurniawan <muhammad_genta_29rpl@student.smktelkom-mlg.sch.id>
2021-03-19 19:06:27 +0700

    first commit

---
+++ pbo2Latihan2/ujiBus2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2Latihan2;
+
+/**
+ *
+ * @author genta
+ */
+public class ujiBus2 {
+            public static void main(String[] args) {
+        // TODO code application logic here
+        Bus2 busMini=new Bus2(10);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(3);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(6);
+        busMini.cetak();
+        
+          
+    }
+}
